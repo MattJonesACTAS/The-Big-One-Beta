c0f5c3e31445dd81bdb6dbcc39fafd611f353d0f
MattJonesACTAS <infomattjns@gmail.com>
2026-05-27 16:00:22 +1000

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1147,7 +1147,7 @@ export default function App() {
                     setRearrested(true);
                     setIsShockForced(true);
                   }}
-                  className="absolute inset-0 w-full h-full rounded-full btn-base flex flex-col items-center justify-center gap-2 sm:gap-3"
+                  className="absolute inset-0 w-full h-full rounded-full btn-base flex flex-col items-center justify-center"
                 >
                   <svg className="absolute inset-0 w-full h-full -rotate-90" viewBox="0 0 300 300">
                     <circle cx="150" cy="150" r="140" fill="none" stroke="currentColor" strokeWidth="6" className="text-neutral-50" />
@@ -1161,8 +1161,8 @@ export default function App() {
                       transition={{ duration: 0.5, ease: "linear" }}
                     />
                   </svg>
-                  <span className="z-10 text-7xl sm:text-[120px] font-bold tracking-tighter leading-none text-emerald-600">ROSC</span>
-                  <span className="z-10 text-[13px] sm:text-[16px] uppercase tracking-widest font-bold text-neutral-400">Tap to rearrest</span>
+                  <span className="z-10 text-[61px] sm:text-[102px] font-bold tracking-tighter leading-none text-emerald-600 translate-y-3 sm:translate-y-4">ROSC</span>
+                  <span className="z-10 text-[13px] sm:text-[16px] uppercase tracking-widest font-bold text-neutral-400 translate-y-3 sm:translate-y-4" style={{ marginTop: '10px' }}>Tap if rearrests</span>
                 </button>
               ) : (
                 <>
