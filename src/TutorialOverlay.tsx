015b2856aff0907c6e7843bf2b83db1c1603a779
MattJonesACTAS <infomattjns@gmail.com>
2026-09-24 21:59:26 +1000

    Add files via upload

--- src/TutorialOverlay.tsx
+++ src/TutorialOverlay.tsx
@@ -27,7 +27,7 @@ const RAW_NODES: Omit<GlobalNode, 'displayNumber'>[] = [
     condition: (s, sf) => s.running && s.currentOverlay === null && !sf
   },
   {
-    id: 'elapsedCorner', type: 'positioned', x: 25, y: 27,
+    id: 'elapsedCorner', type: 'positioned', x: 24, y: 24.5,
     pages: [
       {
         title: 'Elapsed Timer',
