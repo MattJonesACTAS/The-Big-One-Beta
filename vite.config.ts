7b2b03e98db5c6f472d9e45a57e54d8cf4e30173
MattJonesACTAS <infomattjns@gmail.com>
2026-09-26 08:48:27 +1000

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/The-Big-One/',
+  base: '/The-Big-One-Beta/',
   plugins: [
     react(),
     tailwindcss(),
